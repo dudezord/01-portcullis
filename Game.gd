a8a538f64d3c927199410993d33cbd36f207acfe
dudezord <dudezord@protonmail.com>
2022-04-09 01:36:42 +0100

    Add gate health

--- Game.gd
+++ Game.gd
@@ -1,1 +1,4 @@
 extends Node2D
+
+func _process(delta):
+	$GateHealthBar.value = $GateHolder/Gate.health
--- gate/Gate.gd
+++ gate/Gate.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+export(float, 0, 100, 0.1) var health = 100.0
+
 enum Status {
 	Opened,
 	Opening,
